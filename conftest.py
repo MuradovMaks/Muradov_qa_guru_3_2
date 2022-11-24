8f12f84c665b23adb4412514d06def6afbbe6927
Murad <koreantech620@mail.ru>
2022-11-24 13:34:23 +0700

    Изменил структуру файла

--- conftest.py
+++ conftest.py
@@ -4,17 +4,12 @@ import pytest
 from selene.support.shared import browser
 
 
-@pytest.fixture()
+@pytest.fixture(scope="function", autouse=True)
 def browser_open():
-    b = browser.open('https://google.com')
-    print("Браузер открыт")
-    return b
-@pytest.fixture()
-def browser_closed():
-    print("Браузер закрыт")
-
-@pytest.fixture(scope="session")
-def max_res():
     width = browser.config.window_width = 1000
     height = browser.config.window_height = 500
-    return width, height
+    b = browser.open('https://google.com')
+    print("Браузер открыт")
+    yield width, height, b
+    print("Браузер закрыт")
+
--- test_google_search_2.py
+++ test_google_search_2.py
@@ -2,15 +2,10 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have
 
-def test_input_selene(max_res ,browser_open):
+def test_search_text(browser_open):
     browser.element('[name="q"]').should(be.blank).type('Selene').press_enter()
-
-
-def test_search_text(browser_closed):
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
 
-def test_input_selene_negative(browser_open):
+def test_search_text_negative(browser_open):
     browser.element('[name=q]').should(be.blank).type("zgfdgdsgsg").press_enter()
-
-def test_search_text_negative(browser_closed):
     browser.element('[id=search]').should(have.text('abrakadabra'))
