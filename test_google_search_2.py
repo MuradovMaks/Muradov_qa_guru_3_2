b6b53e7feab951c7acafcd1adbd3b5b55f856dda
Murad <koreantech620@mail.ru>
2024-04-12 22:46:23 +0700

    message

--- test_google_search_2.py
+++ test_google_search_2.py
@@ -3,6 +3,7 @@ from selene.support.shared import browser
 from selene import be, have
 
 def test_search_text(browser_open):
+
     browser.element('[name="q"]').should(be.blank).type('Selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
 
